63d4f357a10f397af17869c891d30f03efff1872
Vitalii Kozha <116120008+KozhaVitalii@users.noreply.github.com>
2023-06-03 18:03:22 +0300

    added_base_files

---
+++ src/index.js
@@ -0,0 +1,134 @@
+import './css/common.css';
+// import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+
+
+const breedSelect = new SlimSelect({
+  select: '#breed-select',
+  placeholder: 'Select a breed',
+  settings: {
+    showSearch: false,
+    searchHighlight: true
+  }
+});
+
+
+const catInfoDiv = document.querySelector('.cat-info');
+// const loaderElement = document.querySelector('.loader');
+
+breedSelect.onChange = handleBreedSelectChange;
+// // breedSelect.addEventListener('change', handleBreedSelectChange);
+
+
+fetchBreeds(breedSelect);
+
+fetchCatByBreed(breedId);
+
+
+function handleBreedSelectChange() {
+  const selectedBreedId = breedSelect.selected();
+  console.log('Selected breed ID:', selectedBreedId);
+
+  if (selectedBreedId) {
+    fetchCatByBreed(selectedBreedId)
+      .then(data => {
+        const catInfoMarkup = `
+          <div class="cat-container">
+            <img src="${data.url}" alt="Cat Image" class="cat-image" width="480">
+            <div class="cat-details">
+              <h2 class="cat-breed">${data.breeds[0].name}</h2>
+              <p class="cat-description">${data.breeds[0].description}</p>
+              <p class="cat-temperament">
+                <span class="cat-temperament-key">Temperament:</span>
+                <span class="cat-temperament-value">${data.breeds[0].temperament}</span>
+              </p>
+            </div>
+          </div>
+        `;
+        renderCatInfo(catInfoMarkup);
+        console.log('Result:', data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  } else {
+    renderCatInfo('');
+  }
+}
+
+function renderCatInfo(catInfo) {
+  const catInfoMarkup = catInfo;
+  catInfoDiv.innerHTML = catInfoMarkup;
+}
+
+
+
+
+
+// Работает:
+
+// import './css/common.css';
+// import Notiflix from 'notiflix';
+// import SlimSelect from 'slim-select';
+// import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+
+// const breedSelect = document.querySelector('#breed-select');
+// const catInfoDiv = document.querySelector('.cat-info');
+// const loaderElement = document.querySelector('.loader');
+
+// breedSelect.addEventListener('change', handleBreedSelectChange);
+// fetchBreeds(breedSelect);
+
+// function hideLoader() {
+//   loaderElement.classList.add('hidden');
+//   catInfoDiv.classList.remove('hidden');
+// }
+
+// function showLoader() {
+//   loaderElement.classList.remove('hidden');
+//   catInfoDiv.classList.add('hidden');
+// }
+
+// function showError() {
+//   Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+// }
+
+// hideLoader();
+
+// function handleBreedSelectChange() {
+//   const selectedBreedId = breedSelect.value;
+  
+//   showLoader();
+  
+//   fetchCatByBreed(selectedBreedId)
+//     .then(data => {
+//       const catInfoMarkup = `
+//         <div class="cat-container">
+//           <img src="${data.url}" alt="Cat Image" class="cat-image" width="480">
+//           <div class="cat-details">
+//             <h2 class="cat-breed">${data.breeds[0].name}</h2>
+//             <p class="cat-description">${data.breeds[0].description}</p>
+//             <p class="cat-temperament">
+//               <span class="cat-temperament-key">Temperament:</span>
+//               <span class="cat-temperament-value">${data.breeds[0].temperament}</span>
+//             </p>
+//           </div>
+//         </div>
+//       `;
+//       renderCatInfo(catInfoMarkup);
+//     })
+//     .catch(error => {
+//       console.error(error);
+//       showError();
+//     })
+//     .finally(() => {
+//       hideLoader();
+//     });
+// }
+
+// function renderCatInfo(catInfo) {
+//   const catInfoMarkup = catInfo;
+//   catInfoDiv.innerHTML = catInfoMarkup;
+// }
+
---
+++ src/js/cat-api.js
@@ -0,0 +1,161 @@
+const API_KEY = 'live_5clVSpmR3DcATTc6zmmBYI7V8oKT24zFjWh3sjCCkfju0Yzfw7veF6Dz0HIykaC9';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+
+// Функция выполняет запрос к API для получения списка пород кошек.
+export function fetchBreeds(breedSelect) {
+  const options = {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  };
+
+  const url = `${BASE_URL}/breeds`;
+
+    // showLoader(); // Показываем загрузчик перед отправкой запроса
+    
+    return fetch(url, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch breeds');
+            }
+            return response.json();
+        })
+        .then(breeds => {
+            breedSelect.setData(breeds.map(breed => ({ value: breed.id, text: breed.name })));
+            console.log(breeds);
+            // hideLoader();
+        })
+        .catch(error => {
+            console.error(error);
+            // showError();
+            // hideLoader();
+        })
+        .finally(() => {
+            // hideLoader(); 
+    });
+    }
+
+
+// Функция fetchCatByBreed(breedId) выполняет запрос к API для получения информации о коте определенной породы по Id: 
+export function fetchCatByBreed(breedId) {
+  const options = {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  };
+
+  const url = `${BASE_URL}/images/search?breed_ids=${breedId}`;
+    
+  return fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch breeds');
+      }
+      return response.json();
+    })
+      .then(data => {
+        console.log('Response data:', data);
+        return data[0]; 
+        
+    })
+    .catch(error => {
+      console.error(error);
+        // showError();
+    });
+}
+
+
+fetchCatByBreed('abys')
+  .then(result => {
+    console.log('Result:', result);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+
+// Работает:
+
+// const API_KEY = 'live_5clVSpmR3DcATTc6zmmBYI7V8oKT24zFjWh3sjCCkfju0Yzfw7veF6Dz0HIykaC9';
+// const BASE_URL = 'https://api.thecatapi.com/v1';
+
+// function hideLoaderSelect() {
+//   loaderElement.classList.add('hidden');
+//   breedSelect.classList.remove('hidden');
+// }
+
+// function showLoaderSelect() {
+//   loaderElement.classList.remove('hidden');
+//   breedSelect.classList.add('hidden');
+// }
+
+// // Функция выполняет запрос к API для получения списка пород кошек.
+// export function fetchBreeds(breedSelect) {
+//   const options = {
+//     headers: {
+//       'x-api-key': API_KEY,
+//     },
+//   };
+
+//   const url = `${BASE_URL}/breeds`;
+
+//     // showLoader();
+//     // hideBreedSelect();
+    
+//   return fetch(url, options)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Failed to fetch breeds');
+//       }
+//       return response.json();
+//     })
+//       .then(breeds => {
+//           populateBreedsSelect(breeds, breedSelect);
+//         //   hideLoader();
+//         //   showBreedSelect();
+//     })
+//     .catch(error => {
+//       console.error(error);
+//         showError();
+//     });
+// }
+
+// function populateBreedsSelect(breeds, breedSelect) {
+//   breeds.forEach(breed => {
+//     const option = document.createElement('option');
+//     option.value = breed.id;
+//     option.textContent = breed.name;
+//     breedSelect.appendChild(option);
+//   });
+// }
+
+
+// // Функция fetchCatByBreed(breedId) выполняет запрос к API для получения информации о коте определенной породы по Id: 
+// export function fetchCatByBreed(breedId) {
+//   const options = {
+//     headers: {
+//       'x-api-key': API_KEY,
+//     },
+//   };
+
+//   const url = `${BASE_URL}/images/search?breed_ids=${breedId}`;
+    
+//   return fetch(url, options)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Failed to fetch breeds');
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       return data[0]; 
+//     })
+//     .catch(error => {
+//       console.error(error);
+//         showError();
+//     });
+// }
+
+
