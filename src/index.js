ec7f8ee903bd37768fff4a47cf091b3ec7ce5171
Vitalii Kozha <116120008+KozhaVitalii@users.noreply.github.com>
2023-06-05 12:49:06 +0300

    added_changes_with_select

--- src/index.js
+++ src/index.js
@@ -1,38 +1,59 @@
+// Работает:
+
 import './css/common.css';
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 
-
-const breedSelect = new SlimSelect({
-  select: '#breed-select',
-  placeholder: 'Select a breed',
-  settings: {
-    showSearch: false,
-    searchHighlight: true
-  }
-});
-
-// Получяем ссылку на элемент с классом "cat-info" в переменную catInfoDiv. 
+const breedSelect = document.querySelector('#breed-select');
 const catInfoDiv = document.querySelector('.cat-info');
-// const loaderElement = document.querySelector('.loader');
+const loaderElement = document.querySelector('.loader');
 
-// Устанавиваем обработчик события onChange для объекта breedSelect, который будет вызывать функцию 
-// handleBreedSelectChange при изменении выбранного значения в селекте породы.
-breedSelect.onChange = handleBreedSelectChange;
-// breedSelect.addEventListener('change', handleBreedSelectChange);
-
-// Вызываем функцию fetchBreeds с передачей объекта breedSelect в качестве аргумента.
+breedSelect.addEventListener('change', handleBreedSelectChange);
 fetchBreeds(breedSelect);
 
-// fetchCatByBreed(breedId);
 
+function showLoader() {
+  loaderElement.classList.remove('hidden');
+  catInfoDiv.classList.add('hidden');
+}
+
+function hideLoader() {
+  loaderElement.classList.add('hidden');
+  catInfoDiv.classList.remove('hidden');
+}
+
+function hideSelect() {
+breedSelect.classList.add('hidden');
+}
+
+function showSelect() {
+  breedSelect.classList.remove('hidden');
+}
+
+
+// function showCatInfo() {
+//   catInfoDiv.classList.remove('hidden');
+// }
+
+// function hideCatInfo() {
+//   catInfoDiv.classList.add('hidden');
+// }
+
+function showError() {
+  Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+}
+
+// showLoader()
+  hideSelect();
 
 function handleBreedSelectChange() {
-  const selectedBreedId = breedSelect.selected(); // получаем из селекта выбранный идентификатор породы
-  console.log('Selected breed ID:', selectedBreedId);
-
-  if (selectedBreedId) {
+  const selectedBreedId = breedSelect.value;
+  
+  showLoader();
+  // hideSelect();
+  
+    if (selectedBreedId) {
     fetchCatByBreed(selectedBreedId)
       .then(data => {
         const catInfoMarkup = `
@@ -49,11 +70,22 @@ function handleBreedSelectChange() {
           </div>
         `;
         renderCatInfo(catInfoMarkup);
-        console.log('Result:', data);
+      new SlimSelect({
+          select: '#breed-select',
+          placeholder: 'Select a breed',
+            settings: {
+              showSearch: false,
+              searchHighlight: true
+            }
+      });
       })
       .catch(error => {
         console.error(error);
-      });
+        showError();
+      })
+      .finally(() => {
+        hideLoader();
+    });
   } else {
     renderCatInfo('');
   }
@@ -68,41 +100,12 @@ function renderCatInfo(catInfo) {
 
 
 
-// Работает:
-
-// import './css/common.css';
-// import Notiflix from 'notiflix';
-// import SlimSelect from 'slim-select';
-// import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
-
-// const breedSelect = document.querySelector('#breed-select');
-// const catInfoDiv = document.querySelector('.cat-info');
-// const loaderElement = document.querySelector('.loader');
-
-// breedSelect.addEventListener('change', handleBreedSelectChange);
-// fetchBreeds(breedSelect);
-
-// function hideLoader() {
-//   loaderElement.classList.add('hidden');
-//   catInfoDiv.classList.remove('hidden');
-// }
-
-// function showLoader() {
-//   loaderElement.classList.remove('hidden');
-//   catInfoDiv.classList.add('hidden');
-// }
-
-// function showError() {
-//   Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
-// }
-
-// hideLoader();
 
 // function handleBreedSelectChange() {
 //   const selectedBreedId = breedSelect.value;
   
 //   showLoader();
-  
+
 //   fetchCatByBreed(selectedBreedId)
 //     .then(data => {
 //       const catInfoMarkup = `
@@ -119,6 +122,15 @@ function renderCatInfo(catInfo) {
 //         </div>
 //       `;
 //       renderCatInfo(catInfoMarkup);
+// new SlimSelect({
+//   select: '#breed-select',
+//   placeholder: 'Select a breed',
+//   settings: {
+//     showSearch: false,
+//     searchHighlight: true
+//   }
+// });
+
 //     })
 //     .catch(error => {
 //       console.error(error);
@@ -129,8 +141,127 @@ function renderCatInfo(catInfo) {
 //     });
 // }
 
+
+
+
+// import './css/common.css';
+// // import Notiflix from 'notiflix';
+// import SlimSelect from 'slim-select';
+// import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+
+
+// const catInfoDiv = document.querySelector('.cat-info');
+
+// fetchBreeds().then(breeds => {
+//   const breedSelect = new SlimSelect({
+//     select: '#breed-select',
+//     placeholder: 'Select a breed',
+//     settings: {
+//       showSearch: false,
+//       searchHighlight: true
+//     },
+//     data: breeds.map(breed => ({ value: breed.id, text: breed.name }))
+//   });
+
+//   breedSelect.onChange = handleBreedSelectChange;
+// }).catch(error => {
+//   console.error(error);
+// });
+
+// function handleBreedSelectChange() {
+//   const breedSelect = document.querySelector('#breed-select');
+//   const selectedBreedId = breedSelect.selected();
+//   console.log('Selected breed ID:', selectedBreedId);
+
+//   if (selectedBreedId) {
+//     fetchCatByBreed(selectedBreedId)
+//       .then(data => {
+//         const catInfoMarkup = `
+//           <div class="cat-container">
+//             <img src="${data.url}" alt="Cat Image" class="cat-image" width="480">
+//             <div class="cat-details">
+//               <h2 class="cat-breed">${data.breeds[0].name}</h2>
+//               <p class="cat-description">${data.breeds[0].description}</p>
+//               <p class="cat-temperament">
+//                 <span class="cat-temperament-key">Temperament:</span>
+//                 <span class="cat-temperament-value">${data.breeds[0].temperament}</span>
+//               </p>
+//             </div>
+//           </div>
+//         `;
+//         renderCatInfo(catInfoMarkup);
+//         console.log('Result:', data);
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   } else {
+//     renderCatInfo('');
+//   }
+// }
+
 // function renderCatInfo(catInfo) {
 //   const catInfoMarkup = catInfo;
 //   catInfoDiv.innerHTML = catInfoMarkup;
 // }
 
+
+
+// const breedSelect = new SlimSelect({
+//   select: '#breed-select',
+//   placeholder: 'Select a breed',
+//   settings: {
+//     showSearch: false,
+//     searchHighlight: true
+//   }
+// });
+
+// Получяем ссылку на элемент с классом "cat-info" в переменную catInfoDiv. 
+// const catInfoDiv = document.querySelector('.cat-info');
+// // const loaderElement = document.querySelector('.loader');
+
+// // Устанавиваем обработчик события onChange для объекта breedSelect, который будет вызывать функцию 
+// // handleBreedSelectChange при изменении выбранного значения в селекте породы.
+// breedSelect.onChange = handleBreedSelectChange;
+// // breedSelect.addEventListener('change', handleBreedSelectChange);
+
+// // Вызываем функцию fetchBreeds с передачей объекта breedSelect в качестве аргумента.
+// fetchBreeds(breedSelect);
+
+
+
+// function handleBreedSelectChange() {
+//   const selectedBreedId = breedSelect.selected(); // получаем из селекта выбранный идентификатор породы
+//   console.log('Selected breed ID:', selectedBreedId);
+
+//   if (selectedBreedId) {
+//     fetchCatByBreed(selectedBreedId)
+//       .then(data => {
+//         const catInfoMarkup = `
+//           <div class="cat-container">
+//             <img src="${data.url}" alt="Cat Image" class="cat-image" width="480">
+//             <div class="cat-details">
+//               <h2 class="cat-breed">${data.breeds[0].name}</h2>
+//               <p class="cat-description">${data.breeds[0].description}</p>
+//               <p class="cat-temperament">
+//                 <span class="cat-temperament-key">Temperament:</span>
+//                 <span class="cat-temperament-value">${data.breeds[0].temperament}</span>
+//               </p>
+//             </div>
+//           </div>
+//         `;
+//         renderCatInfo(catInfoMarkup);
+//         console.log('Result:', data);
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   } else {
+//     renderCatInfo('');
+//   }
+// }
+
+// function renderCatInfo(catInfo) {
+//   const catInfoMarkup = catInfo;
+//   catInfoDiv.innerHTML = catInfoMarkup;
+// }
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,5 +1,19 @@
+
+// Работает:
+
 const API_KEY = 'live_5clVSpmR3DcATTc6zmmBYI7V8oKT24zFjWh3sjCCkfju0Yzfw7veF6Dz0HIykaC9';
 const BASE_URL = 'https://api.thecatapi.com/v1';
+
+// function hideLoaderSelect() {
+//   loaderElement.classList.add('hidden');
+//   breedSelect.classList.remove('hidden');
+// }
+
+// function showLoaderSelect() {
+//   loaderElement.classList.remove('hidden');
+//   breedSelect.classList.add('hidden');
+// }
+
 
 
 // Функция выполняет запрос к API для получения списка пород кошек.
@@ -12,29 +26,39 @@ export function fetchBreeds(breedSelect) {
 
   const url = `${BASE_URL}/breeds`;
 
-    // showLoader(); // Показываем загрузчик перед отправкой запроса
+    // showLoader();
+    // hideBreedSelect();
     
-    return fetch(url, options)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch breeds');
-            }
-            return response.json();
-        })
-        .then(breeds => {
-            breedSelect.setData(breeds.map(breed => ({ value: breed.id, text: breed.name })));
-            // console.log(breeds);
-            // hideLoader();
-        })
-        .catch(error => {
-            console.error(error);
-            // showError();
-            // hideLoader();
-        })
-        .finally(() => {
-            // hideLoader(); 
+  return fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch breeds');
+      }
+      return response.json();
+    })
+    .then(breeds => {
+      populateBreedsSelect(breeds, breedSelect);
+      //   hideLoader();
+      // showSelect();
+    })
+    .catch(error => {
+      console.error(error);
+      showError();
     });
-    }
+    // .finally(() => {
+    //   hideLoader();
+    //   showSelect();
+    // });
+}
+
+function populateBreedsSelect(breeds, breedSelect) {
+  breeds.forEach(breed => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.textContent = breed.name;
+    breedSelect.appendChild(option);
+  });
+}
 
 
 // Функция fetchCatByBreed(breedId) выполняет запрос к API для получения информации о коте определенной породы по Id: 
@@ -54,44 +78,26 @@ export function fetchCatByBreed(breedId) {
       }
       return response.json();
     })
-      .then(data => {
-        // console.log('Response data:', data);
-        return data[0]; 
-        
+    .then(data => {
+      return data[0];
     })
     .catch(error => {
       console.error(error);
-        // showError();
+      showError();
     });
+  // .finally(() => {
+  //     hideLoader();
+  //     showSelect();
+  //   });
 }
 
 
-// fetchCatByBreed('abys')
-//   .then(result => {
-//     console.log('Result:', result);
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
-
-
-
-// Работает:
 
 // const API_KEY = 'live_5clVSpmR3DcATTc6zmmBYI7V8oKT24zFjWh3sjCCkfju0Yzfw7veF6Dz0HIykaC9';
 // const BASE_URL = 'https://api.thecatapi.com/v1';
 
-// function hideLoaderSelect() {
-//   loaderElement.classList.add('hidden');
-//   breedSelect.classList.remove('hidden');
-// }
 
-// function showLoaderSelect() {
-//   loaderElement.classList.remove('hidden');
-//   breedSelect.classList.add('hidden');
-// }
-
-// // Функция выполняет запрос к API для получения списка пород кошек.
+// Функция выполняет запрос к API для получения списка пород кошек.
 // export function fetchBreeds(breedSelect) {
 //   const options = {
 //     headers: {
@@ -101,9 +107,34 @@ export function fetchCatByBreed(breedId) {
 
 //   const url = `${BASE_URL}/breeds`;
 
-//     // showLoader();
-//     // hideBreedSelect();
-    
+//      return fetch(url, options)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Failed to fetch breeds');
+//             }
+//             return response.json();
+//         })
+//         .then(breeds => {
+//             breedSelect.setData(breeds.map(breed => ({ value: breed.id, text: breed.name })));
+//             console.log(breeds);
+//         })
+//         .catch(error => {
+//             console.error(error);
+//         })
+//         .finally(() => {
+//     });
+//     }
+
+
+// export function fetchBreeds() {
+//   const options = {
+//     headers: {
+//       'x-api-key': API_KEY,
+//     },
+//   };
+
+//   const url = `${BASE_URL}/breeds`;
+
 //   return fetch(url, options)
 //     .then(response => {
 //       if (!response.ok) {
@@ -111,28 +142,13 @@ export function fetchCatByBreed(breedId) {
 //       }
 //       return response.json();
 //     })
-//       .then(breeds => {
-//           populateBreedsSelect(breeds, breedSelect);
-//         //   hideLoader();
-//         //   showBreedSelect();
-//     })
 //     .catch(error => {
 //       console.error(error);
-//         showError();
 //     });
 // }
 
-// function populateBreedsSelect(breeds, breedSelect) {
-//   breeds.forEach(breed => {
-//     const option = document.createElement('option');
-//     option.value = breed.id;
-//     option.textContent = breed.name;
-//     breedSelect.appendChild(option);
-//   });
-// }
 
-
-// // Функция fetchCatByBreed(breedId) выполняет запрос к API для получения информации о коте определенной породы по Id: 
+// Функция fetchCatByBreed(breedId) выполняет запрос к API для получения информации о коте определенной породы по Id: 
 // export function fetchCatByBreed(breedId) {
 //   const options = {
 //     headers: {
@@ -149,13 +165,22 @@ export function fetchCatByBreed(breedId) {
 //       }
 //       return response.json();
 //     })
-//     .then(data => {
-//       return data[0]; 
+//       .then(data => {
+//         console.log('Response data:', data);
+//         return data[0]; 
+        
 //     })
 //     .catch(error => {
 //       console.error(error);
-//         showError();
+//         // showError();
 //     });
 // }
 
 
+// fetchCatByBreed('abys')
+//   .then(result => {
+//     console.log('Result:', result);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
